e0e9875c6ba6bde8f7bea42ea6fdb8d420ae0e31
Dryganets Sergey <sergey.dryganets@axhive.comdryganets@dryganets-laptop>
2009-03-16 09:41:29 +0600

    rails application sceleton

---
+++ app/controllers/application.rb
@@ -0,0 +1,15 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '68273496e5e490d9a88c2b40212f6e0d'
+  
+  # See ActionController::Base for details 
+  # Uncomment this to filter the contents of submitted sensitive data parameters
+  # from your application log (in this case, all fields with names like "password"). 
+  # filter_parameter_logging :password
+end
